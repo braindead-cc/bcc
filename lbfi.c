96ffafab2d8d3b6ad2721c4ac4b83f039d805d10
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-10 10:57:25 -0400

    lbfi.[ch]: add

---
+++ lbfi.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "lbfi.h"
+#include "types.h"
+
+int
+lbfi_main(char *path)
+{
+	/* get size of file so we can allocate a buffer for it */
+	struct stat st;
+	if (lstat(path, &st) == -1)
+		die("lbfi: error: cannot stat %s:", path);
+	program_data = (char*) calloc(st.st_size + 1, sizeof(char));
+
+	FILE *f;
+	if ((f = fopen(path, "r")) == NULL)
+		die("lbfi: cannot open %s:", path);
+
+	/* copy file data onto buffer */
+	int c = 0;
+	usize i = 0;
+	for (; (c = fget(f)) != EOF; ++i)
+		program_data[i] = c;
+	program_data[i + 1] = '\0';
+
+	/* parse instructions */
+	parse();
+
+
+	/* TODO: warnings, optimizations */
+	/* execute instructions */
+	bf_init();
+
+	struct Instruction *current;
+	for (current = &program[i]; current->next != NULL;
+			current = current->next) {
+		switch (current->command) {
+		case COMMAND_CELL_NULLIFY:
+			bf_cell_nullify();
+			break;
+		case COMMAND_CELL_INC:
+			bf_cell_inc(current->repeat);
+			break;
+		case COMMAND_CELL_DEC:
+			bf_cell_dec(current->repeat);
+			break;
+		case COMMAND_PTR_MOV_INIT:
+			bf_ptr_mov_init();
+			break;
+		case COMMAND_PTR_MOV_L:
+			bf_ptr_mov_l(current->repeat);
+			break;
+		case COMMAND_PTR_MOV_R:
+			bf_ptr_mov_r(current->repeat);
+			break;
+		case COMMAND_LOOP_START:
+			if (*(tape->pointer) == 0) {
+				current = current->next;
+				usize depth = 0;
+				while (depth > 0 || current->command != COMMAND_LOOP_END) {
+					if (current->command == COMMAND_LOOP_START)
+						++depth;
+					else if (current->command == COMMAND_LOOP_END)
+						--depth;
+					current = current->next;
+				}
+			}
+			break;
+		case COMMAND_LOOP_END:
+			if (*(tape->pointer) != 0) {
+				current = current->prev;
+				usize depth = 0;
+				while (depth > 0 || current->command != COMMAND_LOOP_START) {
+					if (current->command == COMMAND_LOOP_END)
+						++depth;
+					else if (current->command == COMMAND_LOOP_START)
+						--depth;
+					current = current->prev;
+				}
+				current = current->prev;
+			}
+			break;
+		case COMMAND_READ_STDIN:
+			bf_read_stdin();
+			break;
+		case COMMAND_PRINT_STDOUT:
+			bf_print_stdout();
+			break;
+		case COMMAND_PRINT_STDERR:
+			bf_print_stderr();
+			break;
+		case COMMAND_PRINT_DEBUG:
+			bf_print_debug();
+			break;
+		case COMMAND_SCAN_L:
+			bf_scan_l();
+			break;
+		case COMMAND_SCAN_R:
+			bf_scan_r();
+			break;
+		case COMMAND_SUICIDE:
+			bf_suicide();
+			break;
+		default:
+			break;
+		}
+	}
+
+	/* cleanup */
+	bf_suicide();
+}
---
+++ lbfi.h
@@ -0,0 +1,7 @@
+#ifndef LBFI_H
+#define LBFI_H
+
+#include <stdio.h>
+int lbfi_main(char *path);
+
+#endif

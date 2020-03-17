43078dd6df37a970170629fb64260d2b690f744a
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-16 20:40:15 -0400

    optimizations: implement sc loops

---
+++ opt-sloops.c
@@ -0,0 +1,47 @@
+/*
+ * optimize scan loops such as '[<]'
+ * or [>] into a single
+ * instruction ('{' or '}', respectively)
+ */
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "instructions.h"
+#include "lbf.h"
+#include "opt-sloops.h"
+#include "status.h"
+
+void
+optimize_sloops(struct Options *opts, struct Instruction *head)
+{
+	usize i = 0;
+	if (opts->verbose)
+		status_init("optimizing scan loops");
+	for (struct Instruction *c = head; c != NULL; c = c->next, ++i) {
+		if (opts->verbose)
+			status_update("optimizing scan loops",
+					i, STATUS_UNKNOWN);
+		if (c->command == '['
+				&& (c->next->command == '<'
+					|| c->next->command == '>')
+				&& c->next->next->command == ']'
+		) {
+			c->command = c->command == '<' ? '{' : '}' ;
+			if (c->next->next->next != NULL) {
+				struct Instruction *tmp = c->next->next->next;
+				free(c->next->next);
+				free(c->next);
+				c->next = tmp;
+				tmp->prev = c;
+			} else {
+				free(c->next->next);
+				free(c->next);
+				c->next = NULL;
+			}
+		}
+	}
+
+	if (opts->verbose)
+		status_complete("optimizing scan loops");
+}
---
+++ opt-sloops.h
@@ -0,0 +1,9 @@
+#ifndef OPT_SLOOPS_H
+#define OPT_SLOOPS_H
+
+#include "instructions.h"
+#include "lbf.h"
+
+void optimize_sloops(struct Options *opts, struct Instruction *head);
+
+#endif

67571f7d397d9ba151c05d613c612c3c4b573841
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-20 21:42:38 -0400

    rwrite tests

--- tests/test.tcl
+++ tests/test.tcl
@@ -32,28 +32,34 @@ proc assert_eq {val1 val2 msg} {
 	}
 }
 
-proc test_eof_chars { } {
+proc test_read_io { } {
 	# check eof characters and linefeeds
 	# see: http://www.hevanet.com/cristofd/brainfuck/tests.b
 
 	# check that default EOF character is 0
-	assert_eq [exec echo | ../lbf lbfi "test-eof.b"] "LB\nLB" \
+	assert_eq [exec echo | ../lbf lbfi "test-io.b"] "LB\nLB" \
 		"EOF (default) == 0"
 
 	# with no change
-	assert_eq [exec echo | ../lbf lbfi -feof-value=none "test-eof.b"] \
+	assert_eq [exec echo | ../lbf lbfi -feof-value=none "test-io.b"] \
 		"LK\nLK" "EOF (-feof-value=none) == none"
 
 	# with -1
-	assert_eq [exec echo | ../lbf lbfi -feof-value=-1 "test-eof.b"] \
+	assert_eq [exec echo | ../lbf lbfi -feof-value=-1 "test-io.b"] \
 		"LA\nLA" "EOF (-feof-value=-1) == -1"
+}
+
+proc test_tape_length { } {
+	# test dynamic allocation
+	assert_eq [exec echo | ../lbf lbfi "test-tape-length.b"] \
+		"#" "dynamic tape allocation"
 }
 
 # output newline
 puts ""
 
 # test functions
-set tests [list [split [info commands test*] " "]]
+set tests [split [info commands test*] " "]
 
 puts "\033\[33m->\033\[0m performing tests on interpeter..."
 hr

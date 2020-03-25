de070efee02177b08811330e09cb652a1d6766fb
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-25 16:17:37 -0400

    config.mk: fix typ0

--- config.mk
+++ config.mk
@@ -70,5 +70,5 @@ LD = gold
 # default ldflags for debug: (null)
 DEBUG_CFLAGS    = -O0 -ggdb
 DEBUG_LDFLAGS   =
-RELEASE_CFLAGS  = -0s -march=native
+RELEASE_CFLAGS  = -Os -march=native
 RELEASE_LDFLAGS = -s

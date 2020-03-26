cb7a7a9573fba480bfc698a394e5018fb500964f
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-26 18:15:34 -0400

    emitc.c: rewrite

--- emitc.c
+++ emitc.c
@@ -35,69 +35,117 @@ emitc(struct Options *opts, struct Instruction *head)
 		break;
 	}
 
-	/* print boilerplate (deliberately obfuscated) */
-	printf("#define _GNU_SOURCE\n");
-	printf("#include <stdio.h>\n");
-	printf("#include <stdlib.h>\n");
-	printf("#include <string.h>\n\n");
-	printf("typedef %s i;\n\n", (char*) &memtype);
-	printf("i *m, t;\nint c = 0;\nunsigned long long s, p = 0;\n");
-	printf("int\nmain(void)\n{\n");
-	printf("m = (i*) malloc(%d * sizeof(i));\n", 100000);
-	printf("if (!m) {\nputs(\"err: cannot allocate mem.\");\nexit(1);\n}\n");
+	/* includes, mov left ('<' command), etc */
+	printf("#define _GNU_SOURCE\n"
+		"#include <stdio.h>\n"        /* for IO */
+		"#include <stdlib.h>\n"       /* for malloc(), exit() */
+		"#include <string.h>\n\n"     /* for memchr(), memrchr() */
+		"typedef %s i;\n\n"
+		"i *m;\n"                  /* memory */
+		"int c = 0;\n"                /* buffer for fgetc() */
+		"unsigned long long sz, p;\n" /* tape size, pointer */
+		"\nvoid ml(int repeat) {\n"
+		"if ((p-repeat)>p);else p-=repeat;}\n",
+		(char*) &memtype);
+
+	/* mov right ('>' command) */
+	printf("void mr(int repeat) {p+=repeat;"
+		"if(p>=sz){");
+	if (opts->fopt_disable_dynamic_alloc) {
+		printf("p-=repeat;");
+	} else {
+		printf("m=realloc(m,sz*=2);"
+			"if (m==NULL){"
+			"perror(\"err: can't realloc mem\");"
+			"exit(1);"
+			"}"
+			"for(unsigned long long z=sz/2;"
+			"z<sz;++z,m[z]=0);");
+	}
+	printf("}}");
+
+	/* read char */
+	printf("void rd(void) {"
+		"c = fgetc(stdin);"
+		"if (c == EOF) {");
+	if (opts->fopt_eof_char == EOF_NO_CHANGE)
+		;
+	else if (opts->fopt_eof_char == EOF_MINUS_1)
+		printf("m[p]=(i)-1;");
+	else if (opts->fopt_eof_char == EOF_ZERO)
+		printf("m[p]=0;");
+	printf("} else {"
+		"m[p] = (i) c;"
+		"}}");
+
+	/* write cell */
+	printf("void wr(FILE *stream) {"
+		"fputc(m[p] %% 255, stream);"
+		"}");
+
+	/* main */
+	printf("\nint\n"
+		"main(void)\n"
+		"{\n"
+		"sz = p = 0;\n"
+		"m = (i*) calloc(%lli, sizeof(i));\n"
+		"if (!m) {\n"
+		"	perror(\"err: can't allocate mem: \");\n"
+		"	exit(1);\n"
+		"}\n", opts->fopt_initial_tape_size);
+
 
 	for (struct Instruction *c = head; c != NULL; c = c->next) {
 		switch (c->command) {
 		case '*':
-			printf("m[p] = 0;\n");
+			printf("m[p]=0;");
 			break;
 		case '+':
-			printf("m[p] += %i;\n",c->repeat);
+			printf("m[p]+=%i;", c->repeat);
 			break;
 		case '-':
-			printf("m[p] -= %i;\n",c->repeat);
+			printf("m[p]-=%i;", c->repeat);
 			break;
 		case '[':
-			printf("while (m[p]) {\n");
+			printf("while(m[p]){");
 			break;
 		case ']':
-			printf("}\n");
+			printf("}");
 			break;
 		case '^':
-			printf("p = 0;\n");
+			printf("p=0;");
 			break;
 		case '<':
-			/* TODO: ptr wrapping */
-			printf("p -= %i;\n",c->repeat);
+			printf("ml(%i);", c->repeat);
 			break;
 		case '>':
-			/* TODO: dynamic allocation */
-			printf("p += %i;\n",c->repeat);
+			printf("mr(%i);", c->repeat);
 			break;
 		case ',':
-			printf("c=fgetc(stdin);m[p]=c==EOF?c:0;\n");
+			printf("rd();");
 			break;
 		case '.':
-			printf("fputc(m[p],stdout);\n");
+			printf("wr(stdout);");
 			break;
 		case '&':
-			printf("fputc(m[p],stderr);\n");
+			printf("wr(stderr);");
 			break;
 		case '{':
-			printf("p-=(i)((void*)&m[p]-memrchr(m,0,m[p]+1));\n");
+			printf("p-=(i)((void*)&m[p]-memrchr(m,0,m[p]+1));");
 			break;
 		case '}':
-			printf("p+=(i)(memchr(&m[p],0,s)-(void*)(&m[p]));\n");
+			printf("p+=(i)(memchr(&m[p],0,s)-(void*)(&m[p]));");
 			break;
 		case '@':
-			printf("free(m);exit(0);\n");
+			printf("free(m);exit(0);");
 			break;
 		case '#':
 			/* deliberately not implemented */
+			break;
 		default:
 			break;
 		}
 	}
 
-	printf("free(m);\nexit(0);\n}\n");
+	printf("free(m);exit(0);}");
 }

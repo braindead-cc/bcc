75cf464c0860c77b24a467f18818e529a5a78f51
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-27 12:58:50 -0400

    tests: revamp

--- tests/test.tcl
+++ tests/test.tcl
@@ -11,15 +11,6 @@ set maxoptlevel 2
 set passed 0
 set failed 0
 
-proc hr { } {
-	#set ttywidth [lindex [split [exec stty size] " "] 1]
-	#for {set i 0} {$i < $ttywidth} {incr i} {
-	#	puts -nonewline "─"
-	#}
-	#puts ""
-	puts "----------------------------------------------------------------"
-}
-
 proc assert_eq {val1 val2 msg} {
 	global passed
 	global failed
@@ -33,7 +24,7 @@ proc assert_eq {val1 val2 msg} {
 	}
 }
 
-proc test_read_io { } {
+proc test_i_read_io { } {
 	# check eof characters and linefeeds
 	# see: http://www.hevanet.com/cristofd/brainfuck/tests.b
 
@@ -50,13 +41,50 @@ proc test_read_io { } {
 		"LA\nLA" "EOF (-feof-value=-1) == -1"
 }
 
-proc test_tape_length { } {
+proc test_i_tape_length { } {
 	# test dynamic allocation
 	assert_eq [exec echo | ../lbf lbfi "test-tape-length.b"] \
 		"#" "dynamic tape allocation"
 }
 
-proc test_comments { } {
+proc test_c_tape_length { } {
+	# test dynamic allocation on compiler
+	exec ../lbf lbfc test-tape-length.b | cc -xc -
+	assert_eq [exec ./a.out] "#" "dynamic tape allocation"
+}
+
+proc test_i_cell_size { } {
+	# ensure default cell size for interpreter
+	# is 8bits
+	assert_eq [exec ../lbf lbfi test-cell-size.b] \
+		"8" "cell size (default) == 8"
+}
+
+proc test_c_cell_size { } {
+	# test cell size for compiler
+	
+	# test default cell size
+	exec ../lbf lbfc test-cell-size.b | cc -xc -
+	assert_eq [exec ./a.out] "8" "cell size (default) == 8"
+
+	# test cell size with cmd options
+	exec ../lbf lbfc -fcell-size=8 test-cell-size.b | cc -xc -
+	assert_eq [exec ./a.out] "8" "cell size (-fcell-size=8) == 8"
+
+	exec ../lbf lbfc -fcell-size=16 test-cell-size.b | cc -xc -
+	assert_eq [exec ./a.out] "16" "cell size (-fcell-size=16) == 16"
+
+	exec ../lbf lbfc -fcell-size=32 test-cell-size.b | cc -xc -
+	assert_eq [exec ./a.out] "32" "cell size (-fcell-size=32) == 32"
+
+	# TODO: fix test-cell-size.b to print
+	# 64 instead of 32 on 64-bit cell size
+
+	#exec ../lbf lbfc -fcell-size=64 test-cell-size.b | cc -xc -
+	#assert_eq [exec ./a.out] "64" "cell size (-fcell-size=64) == 64"
+}
+
+proc test_p_comments { } {
 	# test commenting
 
 	# test default comments
@@ -74,15 +102,26 @@ proc test_comments { } {
 puts ""
 
 # test functions
-set tests [split [info commands test*] " "]
+set intp_tests [split [info commands test_i_*] " "]
+set comp_tests [split [info commands test_c_*] " "]
+set pars_tests [split [info commands test_p_*] " "]
 
 puts "\033\[33m->\033\[0m performing tests on interpeter..."
-hr
-
-foreach test $tests {
+foreach test $intp_tests {
 	$test
 }
-
-hr
-puts "completed [expr $passed+$failed] tests. $passed passed, $failed failed."
 puts ""
+
+puts "\033\[33m->\033\[0m performing tests on compiler..."
+foreach test $comp_tests {
+	$test
+}
+puts ""
+
+puts "\033\[33m->\033\[0m performing tests on parser..."
+foreach test $pars_tests {
+	$test
+}
+puts ""
+
+puts "completed [expr $passed+$failed] tests. $passed passed, $failed failed."
